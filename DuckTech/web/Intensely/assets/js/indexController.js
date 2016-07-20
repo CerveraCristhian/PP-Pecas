821cdb4b08abe14f8dd47d12c56aef09f505a46e
Cristhian Adan Cervera Manriquez <repogitquetzal@gmail.com>
2016-07-20 17:45:12 -0500

    popo
    popo

--- DuckTech/web/Intensely/assets/js/indexController.js
+++ DuckTech/web/Intensely/assets/js/indexController.js
@@ -1,18 +1,21 @@
-var app = angular.module('contenidowebApp', ['directivas']);
+var app = angular.module('contenidowebApp', []);
 app.controller('contenidowebController', function($scope, $http) {
 $scope.contw_descripcion = null;
 $scope.contw_descripcioningles = null;
 $scope.contw_clave = null;
 $scope.contw_contenidowebid = null;
 $scope.contenidoweb = [];
+$scope.Mision = null;
 ActualizarContenido();
 
 function ActualizarContenido(){
 var parametros = {method: 'selectcontenidoweb'}
-$http.post("../../Commun/commun.php",parametros)
+$http.post("../../Admin/Commun/commun.php",parametros)
 .success(function (data)
 {
 $scope.contenidoweb = data;
+$scope.Mision = $scope.contenidoweb.contenidoweb[0].contw_descripcion;
+
 })
 .error(function (error)
 {
